39f171f4e06d580a5855a12e022904bd38d92bdf
Yesha Modi <yesha@Yeshas-MacBook-Pro.local>
2017-01-23 14:23:35 -0500

    Final changes of older version of calculator Ref: https://github.com/Ymodi/MAPD-124-iOS-Calculator

---
+++ CalCulator/CalculatorBrain.swift
@@ -0,0 +1,80 @@
+//
+//  CalculatorBrain.swift
+//  CalCulator
+//
+//  Created by Yesha Modi on 2017-01-22.
+//  Copyright © 2017 Yesha Modi-300895482. All rights reserved.
+//
+
+import Foundation
+
+//func multiply(op1: Double, op2: Double) -> Double{
+//    return op1 * op2
+//}
+class CalculatorBrain
+{
+    private var accmulator = 0.0
+    
+    func setOperand(operand: Double) {
+        accmulator = operand
+    }
+    
+    var operations: Dictionary<String,Operation> = [
+        "∏": Operation.Constant(M_PI), //M_PI
+        "e": Operation.Constant(M_E), //M_E,
+        "√": Operation.UnaryOperation(sqrt),//sqrt,
+        "cos": Operation.UnaryOperation(cos),  //cos
+        "×": Operation.BinaryOperation({ $0 * $1 }),
+        "÷": Operation.BinaryOperation({ $0 / $1 }),
+        "+": Operation.BinaryOperation({ $0 + $1 }),
+        "−": Operation.BinaryOperation({ $0 - $1 }),
+        "=": Operation.Equals
+    ]
+    
+    //enum can have computed var and methods --> Pass by value
+    // Functions are just type like array, Double etc
+    enum Operation {
+        case Constant(Double)
+        case UnaryOperation((Double) -> Double)
+        case BinaryOperation((Double,Double) -> Double)
+        case Equals
+    }
+    
+    func performOperations(symbol: String) {
+        if let operation = operations[symbol] {
+            switch operation {
+            case .Constant(let value):
+                accmulator = value
+            case .UnaryOperation(let fun):
+                accmulator = fun(accmulator)
+            case .BinaryOperation(let function):
+                executePendingOperation()
+                pending = PendingBinaryOperationInfo(binaryFunction: function, firstOperand: accmulator)
+            case .Equals: executePendingOperation()
+            }
+        }
+    }
+    
+    private func executePendingOperation(){
+        if pending != nil{
+            accmulator = pending!.binaryFunction(pending!.firstOperand, accmulator)
+            pending = nil
+        }
+    }
+    
+    private var pending: PendingBinaryOperationInfo?
+    //Struct like enum pass by Value , Pass by refrence lives in a heap
+    struct  PendingBinaryOperationInfo {
+        var binaryFunction: (Double,Double) -> Double
+        var firstOperand: Double
+    }
+    
+    // var result is read only property
+    var result : Double {
+        get{
+            return accmulator
+        }
+    }
+}
+
+
--- CalCulator/ViewController.swift
+++ CalCulator/ViewController.swift
@@ -10,16 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet private weak var display: UILabel! //? = nil
+    //Implicity unwrap optionals
+    private var middleOfTypingNumber = false
+    
+    @IBAction private func touchDigit(_ sender: UIButton )
+    {
+        let digitButton = sender.currentTitle!
+        if middleOfTypingNumber {
+            let textInDisplay = display.text!
+            display.text = textInDisplay + digitButton
+        } else {
+            display.text = digitButton
+        }
+        middleOfTypingNumber = true
+       // print("digitButton \(digitButton) digit")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private var displayButtonValue: Double // making it computed property
+    {
+        get{ return Double(display.text!)! }
+        set{ display.text = String(newValue) //Converting double (newValue) to string
+        }
     }
-
-
+    
+    private var brain = CalculatorBrain()
+    
+    @IBAction private func performOperation(_ sender: UIButton) {
+        if middleOfTypingNumber {
+            brain.setOperand(operand: displayButtonValue)
+        }
+        middleOfTypingNumber = false
+        if let mathematicalSymbol = sender.currentTitle{
+            brain.performOperations(symbol: mathematicalSymbol)
+            displayButtonValue = brain.result
+        }
+    }
+    
 }
 
